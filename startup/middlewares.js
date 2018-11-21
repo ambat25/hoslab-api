5f70be4c329be3fe97e0b9875641ab4f5ea55d15
ambat25 <25ambat25@gmail.com>
2018-11-21 14:37:22 +0100

    added cors

--- startup/middlewares.js
+++ startup/middlewares.js
@@ -1,5 +1,8 @@
 const bodyParser = require('body-parser')
+var cors = require('cors');
+
 module.exports = (app) => {
+    app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({
         extended: true
