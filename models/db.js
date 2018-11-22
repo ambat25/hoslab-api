a670c42a8506006eebd16a567633e89a26a14ef5
ambat25 <25ambat25@gmail.com>
2018-11-22 09:06:59 +0100

    update url to use the one supplied by config

--- models/db.js
+++ models/db.js
@@ -1,7 +1,8 @@
-const PouchDB = require('pouchdb')
+const PouchDB = require('pouchdb');
 PouchDB.plugin(require('pouchdb-erase'));
 PouchDB.plugin(require('pouchdb-find'));
-const config = require('config')
-const db = new PouchDB(`http://admin:admin@localhost:5984/${config.get('db')}`)
+const config = require('config');
 
-module.exports = db
+const db = new PouchDB(`${config.get('db-url')}/${config.get('db')}`);
+
+module.exports = db;
