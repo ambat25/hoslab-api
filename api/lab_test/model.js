ee10e76c5bea2f334d1900fe08d80b8e6fcec88e
ambat25 <25ambat25@gmail.com>
2018-12-04 14:11:16 +0100

    remove stagnant content

--- api/lab_test/model.js
+++
@@ -1,6 +0,0 @@
-const PouchDB = require('pouchdb')
-const config = require('config')
-const LabTest = new PouchDB(`http://admin:admin@localhost:5984/${config.get('test_db')}`)
-console.log(config.get('test_db'))
-
-module.exports = LabTest
